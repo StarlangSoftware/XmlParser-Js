f272830d3c4f99c0105f800ad475084594c3b8c7
Olcay Taner YILDIZ <olcay.yildiz@ozyegin.edu.tr>
2021-10-25 14:42:28 +0300

    Added module.exports

---
+++ index.js
@@ -0,0 +1,7 @@
+const {XmlDocument} = require("./source/XmlDocument");
+const {XmlElement} = require("./source/XmlElement");
+
+module.exports = {
+    XmlDocument : XmlDocument,
+    XmlElement : XmlElement
+}
